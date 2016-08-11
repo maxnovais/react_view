f19e8665fec4db3fcef1daad8d7ed97700e0b60f
Maxwell <maxnovais@gmail.com>
2016-08-11 16:23:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Views
+
+## Apresentação
+
+Projeto para visualizar se o servidor volta a string correspondente do Projeto
+indicado.
---
+++ script.jsx
@@ -0,0 +1,15 @@
+
+var Server = React.createClass({
+  loadServers: function(){
+    $.a
+  }
+  render: function(){
+    return(
+      <div>
+      <h2>Server Name</h2>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<Server />, document.getElementById('content'))
